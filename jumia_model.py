53435c26e3a6fb96d681d25c2138e85a3cbab5ab
GAMA <meraghniakram6@gmail.com>
2022-07-28 18:37:20 +0100

    added models

---
+++ jumia_model.py
@@ -0,0 +1,6 @@
+class Brand:
+    def __init__(self, name, pages):
+
+        self.name = name
+        self.pages = pages
+
--- jumia_scraping.py
+++ jumia_scraping.py
@@ -3,46 +3,70 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from jumia_model import Brand
 
-brands=['bingo','amir']
+brands = [
+    Brand('amir', 1),
+    Brand('bingo', 1),
+    Brand('bref', 1),
+    Brand('brand-brilex', 1),
+    Brand('force-xpress', 1),
+    Brand('life', 1),
+    Brand('test', 1),
+
+]
 
 # url = 'https://www.jumia.dz/jus-boissons-bebe/?page='
+# url='https://www.jumia.dz/epicerie-eau-javel/'+ Brand.name +'/#catalog-listing'
 
-columns = {'product_type':[],'category':[],'sub_category':[],'name': [], 'price': [], 'img url': [],}
-def main():
-    for brand in brands:
-        print('---', brand, '---')
-        url = 'https://www.jumia.dz/produit-lessive/'+ brand +'/#catalog-listing'
-        for page in range(1, 2):
+columns = {'product_type':[],'category':[],'sub_category':[],'brand':[],'name': [], 'price': [],'old_price':[],'percentage':[], 'img url': [],}
+def look_for_brands(brands):
+    for Brand in brands:
+        print('---', Brand.name, '---')
+        url='https://www.jumia.dz/epicerie-eau-javel/'+ Brand.name +'/#catalog-listing'
+        for page in range(1, Brand.pages+1):
             print('---', page, '---')
             r = requests.get(url + str(page))
             soup = BeautifulSoup(r.content, "html.parser")
-
             ancher = soup.find('div', {'class': '-paxs row _no-g _4cl-3cm-shs'}
-                       ).find_all('article', {'class': 'prd _fb col c-prd'})
+                               ).find_all('article', {'class': 'prd _fb col c-prd'})
 
             ancher2= soup.find('div',{'class':'brcbs col16 -pvs'}).find_all('a', {'class': 'cbs'})
 
             for pt in ancher:
                 img = pt.find('a').find(
-                'div', {'class': 'img-c'}).find('img', {'class': 'img'})
+                    'div', {'class': 'img-c'}).find('img', {'class': 'img'})
 
                 name = pt.find('a').find('div', {'class': 'info'}).find(
-                'h3', {'class': 'name'})
+                    'h3', {'class': 'name'})
 
                 price = pt.find('a').find('div', {'class': 'info'}).find(
-                'div', {'class': 'prc'})
+                    'div', {'class': 'prc'})
+
+                old_price = pt.find('a').find('div', {'class': 'info'}).find(
+                    'div', {'class': 's-prc-w'})
 
                 columns['product_type'].append(ancher2[1].text)
                 columns['category'].append(ancher2[2].text)
                 columns['sub_category'].append(ancher2[3].text)
+                columns['brand'].append(ancher2[4].text)
                 columns['name'].append(name.text)
                 columns['price'].append(price.text)
+                if(old_price is not None):
+                    columns['old_price'].append(old_price.contents[0].text)
+                    columns['percentage'].append(old_price.contents[1].text)
+                else:
+                    columns['old_price'].append('')
+                    columns['percentage'].append('')
                 columns['img url'].append(img.get('data-src'))
 
 
         data = pd.DataFrame(columns)
-        data.to_excel('jumia_boisson.xlsx')
+        data.to_excel('jumia_eau_de_javel.xlsx')
+
+def main():
+    look_for_brands(brands)
+
 
 if __name__ == '__main__':
     main()
